baf99c8e4fa5772478b17a752a5690a849de0630
Christoph Hartmüller <CHartlmueller@munichre.com>
2018-10-22 19:51:19 +0200

    add swagger ui :)

--- ChatServerApp/src/main/java/com/netlight/munich/spring/server/app/ChatServerAppApplication.java
+++ ChatServerApp/src/main/java/com/netlight/munich/spring/server/app/ChatServerAppApplication.java
@@ -3,7 +3,10 @@ package com.netlight.munich.spring.server.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class ChatServerAppApplication {
 
 	public static void main(String[] args) {
--- ChatServerBackend/src/main/java/com/netlight/munich/spring/server/backend/ChatServerBackendApplication.java
+++ ChatServerBackend/src/main/java/com/netlight/munich/spring/server/backend/ChatServerBackendApplication.java
@@ -3,7 +3,10 @@ package com.netlight.munich.spring.server.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class ChatServerBackendApplication {
 
 	public static void main(String[] args) {
