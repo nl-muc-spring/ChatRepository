bdd20b9bcc867f48d5f19fe08bf331b4f8fd0079
Christoph Hartmüller <CHartlmueller@munichre.com>
2019-02-05 22:43:38 +0100

    merge master

--- ChatServerApp/src/main/java/com/netlight/munich/spring/server/app/ChatServerAppApplication.java
+++ ChatServerApp/src/main/java/com/netlight/munich/spring/server/app/ChatServerAppApplication.java
@@ -8,11 +8,7 @@ import org.springframework.web.client.RestTemplate;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-<<<<<<< HEAD
-@SpringBootApplication()
-=======
 @SpringBootApplication
->>>>>>> origin/master
 @EnableSwagger2
 public class ChatServerAppApplication {
 
